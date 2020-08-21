4d558d1ba9b0579960e29c9b7fc47fcfe9a34504
Alexander Lomovskiy <a@lomovskiy.com>
2020-08-22 01:25:09 +0300

    Added observer pattern sample

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val locationProvider: GpsLocationProvider = GpsLocationProvider()
+
+    locationProvider.registerListener { latitude, longitude ->
+        println("Log location from listener1: latitude: $latitude; longitude: $longitude")
+    }
+
+    locationProvider.registerListener { latitude, longitude ->
+        println("Log location from listener2: latitude: $latitude; longitude: $longitude")
+    }
+
+    locationProvider.notifyListeners(1.0, 1.0)
+    locationProvider.notifyListeners(2.0, 2.0)
+    locationProvider.notifyListeners(3.0, 3.0)
+
 }
