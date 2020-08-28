e111b0277130ffb39b8cdc6c5418269c4f8e2975
Alexander Lomovskiy <a@lomovskiy.com>
2020-08-29 01:54:32 +0300

    Added template pattern sample

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
 fun main() {
 
-    val navigator = Navigator(HikingStrategy)
-    navigator.buildRoute(1.0, 2.0)
-    navigator.setStrategy(CarStrategy)
-    navigator.buildRoute(3.0, 4.0)
-    navigator.setStrategy(PublicTransportStrategy)
-    navigator.buildRoute(5.0, 6.0)
+    var sportsman: Sportsman = Runner()
+    sportsman.doWorkout()
+    sportsman = Bodybuilder()
+    sportsman.doWorkout()
+    sportsman = Cyclist()
+    sportsman.doWorkout()
 
 }
